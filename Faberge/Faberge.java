8072a226cb8d7da1794aba9eb85fabf14b126fff
Prankiman <69918769+Prankiman@users.noreply.github.com>
2023-05-28 03:10:05 +0200

    commit

--- Faberge/Faberge.java
+++ Faberge/Faberge.java
@@ -1,26 +1,26 @@
 import java.math.BigInteger;
+import java.util.stream.LongStream;
 
 public class Faberge {
-  
-  
- 
-  
- 	public static BigInteger height(BigInteger eggs, BigInteger tries) {
+
+  public static BigInteger height(BigInteger eggs, BigInteger tries) {
     
-    BigInteger minimum = new BigInteger("-1");
+    // att current eggs and tries:  h(n, m), after one try:  h(n-1, m-1) or h(n, m-1)
     
-    System.out.println("beInG eXeCUted");
+    long n = eggs.intValue();
     
-    if(eggs.equals(BigInteger.ONE) || tries.equals(BigInteger.ONE) || tries.equals(BigInteger.ZERO))
-		    return tries;
-  
-    for(BigInteger i = BigInteger.ZERO; i.compareTo(tries) == -1; i.add(BigInteger.ONE)){
-        if(!minimum.equals(new BigInteger("-1")))
-      	  minimum = minimum.min(height(eggs, tries.subtract(BigInteger.ONE)).max(height(eggs.subtract(BigInteger.ONE), tries.subtract(BigInteger.ONE))).add(BigInteger.ONE));
-        else
-        	minimum = height(eggs, tries.subtract(BigInteger.ONE)).max(height(eggs.subtract(BigInteger.ONE), tries.subtract(BigInteger.ONE))).add(BigInteger.ONE);
-      }
-      return minimum;
+    
+    // C(n,k)=C(n−1,k)+C(n−1,k−1)
+    
+    BigInteger height = BigInteger.ZERO, binomial_coeff_part = BigInteger.ONE;
+    
+    for(long i = 0; i < n; i++){
+        binomial_coeff_part = binomial_coeff_part.multiply(tries).divide(BigInteger.valueOf(i+1));
+        height = height.add(binomial_coeff_part);
+        tries = tries.subtract(BigInteger.ONE);
     }
     
+    return height;
+    
+  }
 }
